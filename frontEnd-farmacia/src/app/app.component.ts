0c5923fb3ac99b6066a6bb07c14acb5c04028997
Sergio <elpollo6690@hotmail.com>
2025-03-07 17:17:47 -0500

    pagina home

--- frontEnd-farmacia/src/app/app.component.ts
+++ frontEnd-farmacia/src/app/app.component.ts
@@ -4,9 +4,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,NavbarComponent,HeaderComponent,FooterComponent],
+  imports: [RouterOutlet,NavbarComponent,HeaderComponent,FooterComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
